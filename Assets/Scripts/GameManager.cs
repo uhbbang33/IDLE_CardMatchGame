45028d95774b16272831b536383d766443d56f1a
levell1 <whddnr0317@naver.com>
2023-11-01 16:36:58 +0900

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 using Unity.Profiling;
 using UnityEngine.Events;
 using System.Threading;
+using TMPro;
+using System.Drawing;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,20 +23,20 @@ public class GameManager : MonoBehaviour
     public Text addTxt;
     public Text maxScoreText;
     public Text thisScoreText;
-
+    static public int level;
     public GameObject endPanel;
 
     private RectTransform transaddtxt;
     private Button button;
     byte c;
     public int check;
+    private int tempSpriteNum;
+    private Sprite tempSprite;
 
     float time;
     public float maxTime;
     public float warningTime;
-    bool isRunning = true;
-
-
+    bool isRunning = false;
     public AudioManager audioManager;
 
     public AudioClip match;
@@ -44,8 +46,9 @@ public class GameManager : MonoBehaviour
     public AudioSource audioSource;
 
     public Sprite[] sprites;    // sprite를 Inspector창에서 받기 위한 선언
-    List<GameObject> cardList;  // card들을 담을 cardList, 현재는 card를 섞는데 사용
+    private List<GameObject> cardList;  // card들을 담을 cardList, 현재는 card를 섞는데 사용
     public List<GameObject> namelist;
+    public List<Vector3> points;
 
     public GameObject warningBackground;
     //enum TeamName { 문원정, 조병웅, 김국민, 김종욱, 김희진, 박준형}
@@ -57,45 +60,74 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        tempSprite = sprites[0];
+        tempSpriteNum = 0;
 
         transaddtxt = addTxt.GetComponent<RectTransform>();
         Time.timeScale = 1.0f;
-
         cardList = new List<GameObject>();
         //namelist = new List<GameObject>();
-        Sprite tempSprite = sprites[0];
-        int tempSpriteNum = 0;
 
         // 12개의 카드 생성
         // 카드 sprite를 순차적으로 넣어줌
-        for (int i = 0; i < 12; ++i)
+        if (level == 0)
         {
-            // 카드는 12개 생성되어야 하는데 sprite는 6개
-            // 2개의 카드는 같은 카드여야 하므로
-            if (i % 2 == 0)     // 0, 2, 4, 6, 8, 10 일때만 sprite가 바뀜
+            for (int i = 0; i < 12; ++i)
             {
-                tempSprite = sprites[i / 2];
-                tempSpriteNum = i / 2;
+                // 카드는 12개 생성되어야 하는데 sprite는 6개
+                // 2개의 카드는 같은 카드여야 하므로
+                if (i % 2 == 0)     // 0, 2, 4, 6, 8, 10 일때만 sprite가 바뀜
+                {
+                    tempSprite = sprites[i / 2];
+                    tempSpriteNum = i / 2;
+                }
+
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("Cards").transform;
+                newCard.transform.parent.localScale = new Vector3(0.7f, 0.7f, 1f);
+
+                float x = (i / 3) * 1.4f - 2.1f;
+                float y = (i % 3) * 1.4f - 3.0f;
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                newCard.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = tempSprite;
+                newCard.GetComponent<Card>().spriteNum = tempSpriteNum; // card에 spriteNum 넣어주기
+                cardList.Add(newCard);  // List에 생성한 카드 넣어주기
             }
+        }
+        else
+        {
+            for (int i = 0; i < 24; ++i)
+            {
+                // 카드는 12개 생성되어야 하는데 sprite는 6개
+                // 2개의 카드는 같은 카드여야 하므로
+                if (i % 2 == 0)     // 0, 2, 4, 6, 8, 10 일때만 sprite가 바뀜
+                {
+                    tempSprite = sprites[i / 2];
+                    tempSpriteNum = i / 2;
+                }
 
-            GameObject newCard = Instantiate(card);
-            newCard.transform.parent = GameObject.Find("Cards").transform;
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("Cards").transform;
+                newCard.transform.parent.localScale = new Vector3(0.7f, 0.7f, 1f);
+                newCard.transform.position = new Vector3(0, -1.5f, 1);
 
-            float x = (i / 3) * 1.4f - 2.1f;
-            float y = (i % 3) * 1.4f - 3.0f;
-            newCard.transform.position = new Vector3(x, y, 0);
+                float x = (i / 6) * 1.4f - 2.1f;
+                float y = (i % 6) * 1.1f - 4.2f;
+                points.Add(new Vector3(x, y, 1));
+                
 
-
-            newCard.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = tempSprite;
-            newCard.GetComponent<Card>().spriteNum = tempSpriteNum; // card에 spriteNum 넣어주기
-            cardList.Add(newCard);  // List에 생성한 카드 넣어주기
+                newCard.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = tempSprite;
+                newCard.GetComponent<Card>().spriteNum = tempSpriteNum; // card에 spriteNum 넣어주기
+                cardList.Add(newCard);  // List에 생성한 카드 넣어주기
+            }
         }
 
         // 카드 섞기
-        for(int i = cardList.Count- 1; i> 0; --i)
+        for (int i = cardList.Count- 1; i> 0; --i)
         {
             int randomNum = Random.Range(0, i);
-
+            Debug.Log(cardList.Count);
             // swap
             Vector3 tempPosition = cardList[i].transform.position;
             cardList[i].transform.position = cardList[randomNum].transform.position;
@@ -108,7 +140,16 @@ public class GameManager : MonoBehaviour
         float addy = transaddtxt.anchoredPosition.y;         // addtxt 위치
         addy += 0.5f;                                        // addtxt y값 상승
         transaddtxt.anchoredPosition = new Vector2(0, addy); // addtxt y값 상승
-
+        Vector3 speed = Vector3.zero;
+        for (int i = 0; i < points.Count; i++)
+        {
+            cardList[i].transform.position =
+            //Vector3.MoveTowards(cardList[i].transform.position, points[i], 0.01f);
+            //Vector3.SmoothDamp(cardList[i].transform.position, points[i], ref speed, 0.05f);
+            Vector3.Lerp(cardList[i].transform.position, points[i], 0.05f);
+            //Vector3.Slerp(cardList[i].transform.position, points[i], 0.01f);
+        }
+        
         c -= 1;                                              // 글자 색상 투명하게
         addTxt.color = new Color32(255, 0, 0, c);            // 글자 색상 투명하게
 
@@ -122,8 +163,6 @@ public class GameManager : MonoBehaviour
 
         if (time > maxTime)
             GameEnd();
-
-
 
         timeTxt.text = time.ToString("N2");
     }
@@ -140,8 +179,7 @@ public class GameManager : MonoBehaviour
             string info = firstCard.GetComponentInChildren<SpriteRenderer>().sprite.name;   // sprite의 이름 rtanx info에 저장
             check = int.Parse(info.Substring(info.Length - 1)) -1;  // rtanx 의 x부분 자르기, int 로 변형
             // 배열은 0부터 시작하므로 -1
-
-            //check = firstCard.GetComponent<Card>().spriteNum;
+            // check = firstCard.GetComponent<Card>().spriteNum;
 
             namelist[check].SetActive(true);            // Active True
             StartCoroutine(nActiveFalse(check));
@@ -235,9 +273,7 @@ public class GameManager : MonoBehaviour
 
     public void RetryGame()
     {
-       // SceneManager.LoadScene("GameScene");
-        SceneManager.LoadScene("MainScene1");
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void GoHomeBtn()
--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -7,9 +7,11 @@ using UnityEngine.SceneManagement;
 public class StartBtn : MonoBehaviour
 {
     private Button button;
+    
 
     void Awake()
     {
+        
         //PlayerPrefs.DeleteAll();
         if (PlayerPrefs.HasKey("bestscore"))
         {
@@ -19,16 +21,19 @@ public class StartBtn : MonoBehaviour
 
     public void GameStartE()
     {
+        GameManager.level = 0;
         SceneManager.LoadScene("MainScene1");
     }
 
     public void GameStartN()
-    {  
+    {
+        GameManager.level = 1;
         SceneManager.LoadScene("MainScene2");
     }
 
     public void GameStartH()
     {
+        GameManager.level = 2;
         SceneManager.LoadScene("MainScene3");
     }
 
